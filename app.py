4dc7fae951156eea560fa97e40f3efd2dded4d15
robertsc2 <robertsc2@stu.otc.school.nz>
2025-07-08 03:53:31 +1200

    3:53 am commit - Bedtime for me

--- app.py
+++ app.py
@@ -3,16 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template("index.html")
+
 
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
